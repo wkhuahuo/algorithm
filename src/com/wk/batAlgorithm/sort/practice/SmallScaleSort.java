ce02426661bf2e7e583afc0a3e3686341835e385
wkhuahuo <wkhuahuo@163.com>
2016-10-11 21:09:36 +0800

    目录更改

--- src/com/wk/batAlgorithm/sort/practice/SmallScaleSort.java
+++ src/com/wk/batAlgorithm/sort/practice/SmallScaleSort.java
@@ -4,79 +4,64 @@ package com.wk.batAlgorithm.sort.practice;
  * Created by wkhuahuo on 10/11/16.
  */
 public class SmallScaleSort {
+
     public int[] sortElement(int[] A, int n, int k) {
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n-k;i++){
 
-
+            adjustHeap(A, 0, k-1,i);
 
         }
-
-
-        return A;
-
-    }
-    private int[] getFirstMin(int A[], int left,int right){
-        int length=right;
-        int tmp = 0;
-
-        while(length>left){//当前未排序的长度
-            if((length%2)==1){
-                if(A[length]>A[(length-1)/2]){
-                    tmp = A[length];
-                    A[length] = A[(length-1)/2];
-                    A[(length-1)/2] = tmp;
-                }
-
-            }else{
-                if(A[length]>A[(length-2)/2]){
-                    tmp = A[length];
-                    A[length] = A[(length-2)/2];
-                    A[(length-2)/2] = tmp;
-                }
-            }
-            length--;
-        }
-
-        return A;
+    return A;
 
     }
 
-    private int[] headSort(int A[], int left,int right){
+    private int[] adjustHeap(int[] A, int start, int end,int base){//小根堆调整
 
-        if(A==null ||A.length==1){
+        int target = A[start];
+        if(start>end){
             return A;
         }
-        int end = right;//带排序的最后一个数
-        int length = right-left+1;
-        int tmp=0;
-        while(end>left){//记录最后一个未排好序的位置
-            length = end;
-            while(length>0){//当前未排序的长度
-                if((length%2)==1){
-                    if(A[length]>A[(length-1)/2]){
-                        tmp = A[length];
-                        A[length] = A[(length-1)/2];
-                        A[(length-1)/2] = tmp;
+        int rootIndex = start;
+        int child = 0;
+        int tmp;
+        for(;rootIndex*2+1<=end;){
+            child = rootIndex*2+1;
+            if(child+1<=end){//若果说最后一个节点有右节点
+                //寻找较小的孩子
+                if(A[child+1]>A[child]){//左较小
+                    if(A[child+base] <A[rootIndex+base]){//当前根节点与左孩子节点比较
+                        swap(A, child+base, rootIndex+base);
+                        rootIndex = child;
+                    }else {
+                        rootIndex = child;
                     }
 
-                }else{
-                    if(A[length]>A[(length-2)/2]){
-                        tmp = A[length];
-                        A[length] = A[(length-2)/2];
-                        A[(length-2)/2] = tmp;
+                }else {//右孩子比较小
+                    if(A[child+1+base] >A[rootIndex+base]){//与右孩子节点比较
+                        swap(A, child+base, rootIndex+base);
+                        rootIndex = child+1;
+                    }else {
+                        rootIndex = child;
                     }
+
                 }
-                length--;
+            }else if(child<=end){//如果没有只与左孩子节点比较
+
+                if(A[child+base] <A[rootIndex+base]){//如果比较大交换
+                    swap(A, child+base, rootIndex+base);
+                    rootIndex = child;
+                }else {
+                    rootIndex = child;
+                }
             }
-            tmp = A[0];
-            A[0] = A[end];
-            A[end] = tmp;
-            end--;
         }
         return A;
     }
-
-
-
+    private void swap(int[] A, int a, int b){
+        int tmp;
+        tmp =A[a];
+        A[a] = A[b];
+        A[b] = tmp;
+    }
 }
