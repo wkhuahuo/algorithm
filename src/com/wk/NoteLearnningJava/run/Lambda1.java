6da2dfd7a8a7fc3b0048a7861803fbfcb92687a7
wkhuahuo <wkhuahuo@163.com>
2016-11-15 14:10:42 +0800

    Lambda表达式测试

---
+++ src/com/wk/NoteLearnningJava/run/Lambda1.java
@@ -0,0 +1,8 @@
+package com.wk.NoteLearnningJava.run;
+
+/**
+ * Created by wkhuahuo on 16/10/14.
+ */
+public class Lambda1 {
+
+}
---
+++ src/com/wk/NoteLearnningJava/test/LambdaTest1.java
@@ -0,0 +1,22 @@
+package com.wk.NoteLearnningJava.test;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by wkhuahuo on 16/10/14.
+ */
+public class LambdaTest1 {
+    @Test
+    public void testLambda(){
+
+        List<String> names = Arrays.asList("Justin", "Monica", "Irene");
+        for(String name: names){
+            System.out.println(name);
+        }
+
+
+    }
+}
