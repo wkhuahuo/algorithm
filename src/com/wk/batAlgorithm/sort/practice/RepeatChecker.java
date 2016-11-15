cc9a545bec10ef69f5137885dc184a40fdaabf44
wkhuahuo <wkhuahuo@163.com>
2016-11-15 22:53:23 +0800

    更改目录结构，实现堆排序

---
+++ src/com/wk/batAlgorithm/sort/practice/RepeatChecker.java
@@ -0,0 +1,18 @@
+package com.wk.batAlgorithm.sort.practice;
+
+/**
+ * Created by wkhua on 16/11/15.
+ */
+public class RepeatChecker {
+    public boolean checkDuplicate(int[] a, int n) {
+        // write code here
+
+
+        return false;
+    }
+    private int[] HeapSort(int[] a){
+
+        return a;
+    }
+
+}
---
+++ test/com/wk/batAlgorithm/test/practice/RepeatCheckerTest.java
@@ -0,0 +1,22 @@
+package com.wk.batAlgorithm.test.practice;
+
+import org.junit.Test;
+
+/**
+ * Created by wkhua on 16/11/15.
+ */
+public class RepeatCheckerTest {
+    /*
+    * 请设计一个高效算法，判断数组中是否有重复值。必须保证额外空间复杂度为O(1)。
+给定一个int数组A及它的大小n，请返回它是否有重复值。
+测试样例：
+[1,2,3,4,5,5,6],7
+返回：true
+    * */
+    @Test
+    public void testCheckDuplicate(int[] a, int n) {
+        // write code here
+
+    }
+
+}
