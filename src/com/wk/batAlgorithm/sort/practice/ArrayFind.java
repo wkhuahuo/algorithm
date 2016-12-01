8642d31998a8a8f7db59486a3becc5404b18f15a
wkhuahuo <wkhuahuo@163.com>
2016-12-01 22:55:07 +0800

    寻找已排序数组中的某个数是否存在

--- src/com/wk/batAlgorithm/sort/practice/ArrayFind.java
+++ src/com/wk/batAlgorithm/sort/practice/ArrayFind.java
@@ -7,8 +7,18 @@ public class ArrayFind {
 
     public boolean findX(int[][] mat, int n, int m, int x) {
 
-
-        return true;
+        int tmpRow =n-1;
+        int tmpColumn =0;
+        while(tmpColumn<m && tmpRow>=0){
+            if(mat[tmpRow][tmpColumn]<x){
+                tmpColumn++;
+            }else if(mat[tmpRow][tmpColumn]>x){
+                tmpRow--;
+            }else{
+                return true;
+            }
+        }
+        return false;
 
     }
 }
