e4f43ec0a4d17369258108bb0b6092185ee0898a
wkhuahuo <wkhuahuo@163.com>
2016-12-01 21:46:50 +0800

    寻找已排序数组中的某个数是否存在

---
+++ src/com/wk/batAlgorithm/sort/practice/ArrayFind.java
@@ -0,0 +1,14 @@
+package com.wk.batAlgorithm.sort.practice;
+
+/**
+ * Created by wkhua on 16/12/1.
+ */
+public class ArrayFind {
+
+    public boolean findX(int[][] mat, int n, int m, int x) {
+
+
+        return true;
+
+    }
+}
---
+++ test/com/wk/batAlgorithm/test/practice/ArrayFindTest.java
@@ -0,0 +1,7 @@
+package com.wk.batAlgorithm.test.practice;
+
+/**
+ * Created by wkhua on 16/12/1.
+ */
+public class ArrayFindTest {
+}
