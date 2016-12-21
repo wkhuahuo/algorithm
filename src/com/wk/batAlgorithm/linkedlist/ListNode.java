e4145cbbb173b28b47cb44df9fa82d93b0545c65
wkhuahuo <wkhuahuo@163.com>
2016-12-21 13:28:25 +0800

    链表练习

---
+++ src/com/wk/batAlgorithm/linkedlist/ListNode.java
@@ -0,0 +1,12 @@
+package com.wk.batAlgorithm.linkedlist;
+
+/**
+ * Created by wkhua on 16/12/21.
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
