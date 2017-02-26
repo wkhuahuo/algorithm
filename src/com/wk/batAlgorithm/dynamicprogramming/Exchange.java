e614365bc4b7cd0604ff22dac6bb21b7a4319297
wkhuahuo <wkhuahuo@163.com>
2017-02-26 18:45:34 +0800

    新增了junit包

---
+++ src/com/wk/batAlgorithm/dynamicprogramming/Exchange.java
@@ -0,0 +1,20 @@
+package com.wk.batAlgorithm.dynamicprogramming;
+
+/**
+ * 链接：https://www.nowcoder.com/courses/1/12/2
+ 来源：牛客网
+
+ 有数组penny，penny中所有的值都为正数且不重复。每个值代表一种面值的货币，每种面值的货币可以使用任意张，再给定一个整数aim(小于等于1000)代表要找的钱数，求换钱有多少种方法。
+ 给定数组penny及它的大小(小于等于50)，同时给定一个整数aim，请返回有多少种方法可以凑成aim。
+ 测试样例：
+ [1,2,4],3,3
+ 返回：2
+ * Created by wkhuahuo on 2017/2/26.
+ */
+public class Exchange {
+    public int countWays(int[] penny, int n, int aim) {
+        // write code here
+
+        return 0;
+    }
+}
---
+++ test/com/wk/batAlgorithm/dynamicprogramming/GetChangeTest.java
@@ -0,0 +1,12 @@
+package com.wk.batAlgorithm.dynamicprogramming;
+
+
+/**
+ * Created by wkhuahuo on 2017/2/26.
+ */
+public class GetChangeTest {
+
+    public void test(){
+
+    }
+}
