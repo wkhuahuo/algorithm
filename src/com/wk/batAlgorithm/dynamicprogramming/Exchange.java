3c315275d406fc5b1fdcb33746a077f3c93c27fc
wkhuahuo <wkhuahuo@163.com>
2017-03-05 13:47:40 +0800

    新增面试中的知识点

--- src/com/wk/batAlgorithm/dynamicprogramming/Exchange.java
+++ src/com/wk/batAlgorithm/dynamicprogramming/Exchange.java
@@ -1,5 +1,8 @@
 package com.wk.batAlgorithm.dynamicprogramming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 链接：https://www.nowcoder.com/courses/1/12/2
  来源：牛客网
@@ -13,8 +16,80 @@ package com.wk.batAlgorithm.dynamicprogramming;
  */
 public class Exchange {
     public int countWays(int[] penny, int n, int aim) {
-        // write code here
 
-        return 0;
+        return coins(penny,n,aim);
+    }
+    public int coins(int[] penny, int n,int aim){
+        if(penny ==null || aim<0 || penny.length == 0 ){
+            return 0;
+        }
+        int[][] map = new int[penny.length][aim+1];
+        return process(penny, 0, aim ,map);
+    }
+    private int process(int[] penny,int n,int aim,int[][] map){
+
+        for(int i=0;i<penny.length;i++){
+            map[i][0] = 1;
+        }
+        for(int j=0;j<=aim;j++){
+            map[0][j] = j%penny[0] == 0?1:0;
+        }
+        for(int i=1;i<penny.length;i++){
+            for(int j=1;j<=aim;j++){
+                if(j-penny[i]<0){
+                    map[i][j] = map[i-1][j];
+                }else {
+                    map[i][j] = map[i - 1][j] + map[i][j - penny[i]];
+                }
+            }
+        }
+
+        return map[penny.length-1][aim];
+    }
+
+
+
+
+    public int coins1(int[] penny,int n,int aim){
+        if(penny ==null || aim<0 || penny.length == 0 ){
+            return 0;
+        }
+        return coinsProcess(penny, 0, aim);
+    }
+    private int coinsProcess(int[] penny,int n,int aim){
+        int res = 0;
+        if(penny.length == n){
+            res =aim == 0 ? 1:0;
+        }else {
+            for (int i = 0; penny[n]*i <= aim; i++ ) {
+                res += coinsProcess(penny,n+1,aim-penny[n]*i);
+            }
+        }
+        return res;
+    }
+
+    public int coins2(int[] penny, int n, int aim){
+        if(penny ==null || aim<0 || penny.length == 0 ){
+            return 0;
+        }
+        int[][] map = new int[penny.length][aim+1];
+        return coinsProcess(penny, 0, aim , map);
+    }
+    private int coinsProcess(int[] penny,int n,int aim,int[][] map){
+        int res = 0;
+        if(penny.length == n){
+            res =aim == 0 ? 1:0;
+        }else {
+            int mapValue =0;
+            for (int i = 0; penny[n]*i <= aim; i++ ) {
+                mapValue = map[n][aim-penny[n]*i] ;
+                if(mapValue !=0){
+                    res +=mapValue;
+                }else {
+                    res += coinsProcess(penny, n + 1, aim - penny[n] * i);
+                }
+            }
+        }
+        return res;
     }
 }
---
+++ src/com/wk/batAlgorithm/dynamicprogramming/MinCost.java
@@ -0,0 +1,18 @@
+package com.wk.batAlgorithm.dynamicprogramming;
+
+/**
+ * 对于两个字符串A和B，我们需要进行插入、删除和修改操作将A串变为B串，
+ * 定义c0，c1，c2分别为三种操作的代价，请设计一个高效算法，求出将A串变为B串所需要的最少代价。
+ * 给定两个字符串A和B，及它们的长度和三种操作代价，请返回将A串变为B串所需要的最小代价。
+ * 保证两串长度均小于等于300，且三种代价值均小于等于100。
+ * 测试样例：
+ * "abc",3,"adc",3,5,3,100
+ * 返回：8
+ * Created by wkhuahuo on 2017/3/1.
+ */
+public class MinCost {
+    public int findMinCost(String A, int n, String B, int m, int c0, int c1, int c2) {
+
+        return 0;
+    }
+}
