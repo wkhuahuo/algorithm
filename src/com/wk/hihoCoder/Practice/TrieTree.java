97785657e1828172fdc56a4437ffe3d1b57ce7cf
wkhuahuo <wkhuahuo@163.com>
2017-04-01 09:20:54 +0800

    新增算法

---
+++ src/com/wk/hihoCoder/Practice/TrieTree.java
@@ -0,0 +1,14 @@
+package com.wk.hihoCoder.Practice;
+
+/**
+ * Created by wkhuahuo on 2017/3/31.
+ */
+public class TrieTree {
+
+}
+
+class TrieNode{
+    int val;
+    TrieNode right;
+    TrieNode son;
+}
