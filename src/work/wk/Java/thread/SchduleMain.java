1827f5f472f7cd0a5bdd7d35d1063efa45e981c9
wangkai43 <wangkai43@meituan.com>
2017-07-06 17:32:14 +0800

    新增对java基础特性学习的Demo

---
+++ src/work/wk/Java/thread/SchduleMain.java
@@ -0,0 +1,29 @@
+package work.wk.Java.thread;
+
+import sun.jvmstat.perfdata.monitor.CountedTimerTaskUtils;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by wangkai43 on 2017/7/5.
+ */
+public class SchduleMain {
+    public static void main(String[] args) {
+
+        SchduleThreadPoolEx stpe = new SchduleThreadPoolEx();
+
+        ScheduledThreadPoolExecutor sepe  = new ScheduledThreadPoolExecutor(10);
+
+     //  sepe.scheduleAtFixedRate(new TimerWorker(stpe),0,2, TimeUnit.SECONDS);
+
+        sepe.scheduleWithFixedDelay(new TimerWorker(stpe),0,5, TimeUnit.SECONDS);
+
+
+
+        sepe.getActiveCount();
+        System.out.println("taskCount "+sepe.getTaskCount());
+      //  stpe.notifyothers();
+
+    }
+}
